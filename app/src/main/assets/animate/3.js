5739d302d5103eaaa4cf3be1e0936c080ab94209
grandiloquent <1785907865@qq.com>
2023-12-29 17:20:42 +0800

    更新

--- app/src/main/assets/animate/3.js
+++ app/src/main/assets/animate/3.js
@@ -1,25 +1,7 @@
 (() => {
     const str = `"'`;
     console.log(str.split('')
-        .map(x => {
-            return `<Key android:codes="${x.codePointAt(0)}" android:keyLabel="${x}"/>`;
-        }).join('\n'));
-})();
-
-
-(() => {
-
-    console.log([...new Array(26).keys()]
-        .map(key => key + 97)
-        .map(x => {
-            let f = '';
-            let s = "";
-            if ((x - 96 ) % 5 === 0)
-                s = "</Row>";
-            else if ((x - 96 - 1) % 5 === 0)
-                f = "<Row>";
-
-
-            return `${f}<Key android:codes="${x}" android:keyLabel="${String.fromCodePoint(x)}"/>${s}`;
-        }).join('\n'));
-})();
+    .map(x=>{
+        return `<Key android:codes="${x.codePointAt(0)}" android:keyLabel="${x}"/>`;
+    }).join('\n'));
+})();s

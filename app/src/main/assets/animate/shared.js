2ac7dc83dbfecf75536a0fc94ebae4672f6864af
grandiloquent <1785907865@qq.com>
2024-01-06 22:34:18 +0800

    更新页面

--- app/src/main/assets/animate/shared.js
+++ app/src/main/assets/animate/shared.js
@@ -977,3 +977,14 @@ function removeSubstring(strings, prefix, suffix) {
     }
     return `${strings.substring(0, start)}${strings.substring(end)}`;
 }
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }

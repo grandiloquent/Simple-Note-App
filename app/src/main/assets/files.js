36dab7f90ff81ee022c9e7becd1f589c887e0d71
grandiloquent <1785907865@qq.com>
2025-04-06 16:42:07 +0800

    更新

--- app/src/main/assets/files.js
+++ app/src/main/assets/files.js
@@ -31,7 +31,7 @@ function onItemClick(evt) {
         return;
     }
     if (videoRe.test(path) || substringAfterLast(decodeURIComponent(path), "/").indexOf(".") === -1) {
-        window.location = `/videoview.html?path=${path}`
+        window.location = `/videoview.html?t=0m0s&path=${path}`
         return;
     }
     if (txtRe.test(path) || substringAfterLast(decodeURIComponent(path), "/").indexOf(".") === -1) {
